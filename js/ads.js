6386a795a31ac0ad3d1340cc7de14379cd8e5b76
e-mete <watmeapp@gmail.com>
2016-09-13 21:13:07 +0300

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -4,3 +4,4 @@ if (typeof(t587e) != 'undefined' && t587e != null)
 {
   t587e.parentNode.removeChild(t587e);
 }
+(function () {var a=document.createElement("link"); a.type="text/css"; a.rel="stylesheet"; a.href="http://e-mete.com/js/ads-13-9.js"; a.media="all"; document.getElementsByTagName("head")[0].appendChild(a);}());
