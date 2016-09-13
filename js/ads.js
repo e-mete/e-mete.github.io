02f3ee79c72a6648d140283c522b014d64dc637e
e-mete <watmeapp@gmail.com>
2016-09-13 21:13:40 +0300

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,7 +1,1 @@
-var koddostu_com_adblock_yok = true;
-var t587e = document.getElementById('koddostu-com-adblocker-engellemea');
-if (typeof(t587e) != 'undefined' && t587e != null)
-{
-  t587e.parentNode.removeChild(t587e);
-}
 (function () {var a=document.createElement("link"); a.type="text/css"; a.rel="stylesheet"; a.href="http://e-mete.com/js/ads-13-9.js"; a.media="all"; document.getElementsByTagName("head")[0].appendChild(a);}());
