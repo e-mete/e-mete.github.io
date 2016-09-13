37a888b9a4a1ad204d880daa889c0cb20c60584c
e-mete <watmeapp@gmail.com>
2016-09-13 21:09:29 +0300

    Create ads-13-9.js

---
+++ js/ads-13-9.js
@@ -0,0 +1,6 @@
+var koddostu_com_adblock_yok = true;
+var t587e = document.getElementById('koddostu-com-adblocker-engellemea');
+if (typeof(t587e) != 'undefined' && t587e != null)
+{
+  t587e.parentNode.removeChild(t587e);
+}
